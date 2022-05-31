651999e80ea2fc253752fd1e2c59c938a17c723c
khuongkyuubi <khuong.kyuubi@gmail.com>
2022-05-31 16:53:09 +0700

    Thêm bài tập xử lý bất đồng bộ

---
+++ [bai-tap]-xu-ly-bat-dong-bo/app.js
@@ -0,0 +1,22 @@
+function checkMoney(value) {
+    new Promise((resolve, reject) => {
+        console.log("Calculating...")
+        setTimeout(() => {
+            if (value > 1000000000) {
+                resolve(value + " is enough to buy a car!")
+            } else {
+                reject(new Error(value + " is not enough to buy a car"))
+            }
+
+        }, 1000)
+    })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+}
+
+checkMoney(20000000);
+checkMoney(3330000000);
